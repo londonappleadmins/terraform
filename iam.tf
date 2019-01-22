c354c6b7faa500c8bbea1b5e305411dd5e72008e
Graham Gilbert <graham@grahamgilbert.com>
2019-01-22 19:13:46 +0000

    IAM user for deploying to the bucket

---
+++ iam.tf
@@ -0,0 +1,23 @@
+resource "aws_iam_user" "laa_s3_deploy" {
+  name = "laa_s3_deploy"
+}
+
+data "aws_iam_policy_document" "laa_s3_policy_data" {
+  statement {
+    sid = "1"
+
+    actions = [
+      "s3:*",
+    ]
+
+    resources = [
+      "${aws_s3_bucket.www.arn}",
+      "${aws_s3_bucket.www.arn}/*",
+    ]
+  }
+}
+
+resource "aws_iam_policy" "laa_deploy_policy" {
+  name   = "laa_deploy_policy"
+  policy = "${data.aws_iam_policy_document.laa_s3_policy_data.json}"
+}
