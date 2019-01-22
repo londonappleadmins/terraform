fbdabb36435e25f182fb39e72c49c4728d2d97f4
Graham Gilbert <graham@grahamgilbert.com>
2019-01-22 19:26:40 +0000

    allow cloudfront invalidations

--- iam.tf
+++ iam.tf
@@ -15,13 +15,34 @@ data "aws_iam_policy_document" "laa_s3_policy_data" {
       "${aws_s3_bucket.www.arn}/*",
     ]
   }
+
+  statement {
+    actions = [
+      "acm:ListCertificates",
+      "cloudfront:GetDistribution",
+      "cloudfront:GetStreamingDistribution",
+      "cloudfront:GetDistributionConfig",
+      "cloudfront:ListDistributions",
+      "cloudfront:ListCloudFrontOriginAccessIdentities",
+      "cloudfront:CreateInvalidation",
+      "cloudfront:GetInvalidation",
+      "cloudfront:ListInvalidations",
+      "elasticloadbalancing:DescribeLoadBalancers",
+      "iam:ListServerCertificates",
+      "sns:ListSubscriptionsByTopic",
+      "sns:ListTopics",
+      "waf:GetWebACL",
+      "waf:ListWebACLs",
+    ]
+
+    resources = ["*"]
+  }
 }
 
 resource "aws_iam_policy" "laa_deploy_policy" {
   name   = "laa_deploy_policy"
   policy = "${data.aws_iam_policy_document.laa_s3_policy_data.json}"
 }
-
 
 resource "aws_iam_user_policy_attachment" "test-attach" {
   user       = "${aws_iam_user.laa_s3_deploy.name}"
