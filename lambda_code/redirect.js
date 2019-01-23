cc24496dd08b8bed66b2c836d3c1273233c372e6
Graham Gilbert <graham@grahamgilbert.com>
2019-01-23 02:51:04 +0000

    www_redirect

--- lambda_code/redirect.js
+++ lambda_code/redirect.js
@@ -13,22 +13,38 @@ let defaultDocument = 'index.html';
 
 exports.handler = (event, context, callback) => {
     const request = event.Records[0].cf.request;
+    const headers     = request.headers;
+    const host = headers['host'][0].value;
+    if(host.startsWith('www.')){
+        const location = "https://" + host.replace("www.", "") + request.uri;
+        const response = {
+            status: '302',
+            statusDescription: 'Found',
+            headers: {
+                location: [{
+                    key: 'Location',
+                    value: location
+                }]
+            }
+        };
+        callback(null, response);
+    } else {
+        if(request.uri != "/") {
+            let paths = request.uri.split('/');
+            let lastPath = paths[paths.length - 1];
+            let isFile = lastPath.split('.').length > 1;
 
-    if(request.uri != "/") {
-        let paths = request.uri.split('/');
-        let lastPath = paths[paths.length - 1];
-        let isFile = lastPath.split('.').length > 1;
+            if(!isFile) {
+                if(lastPath != "") {
+                    request.uri += "/";
+                }
 
-        if(!isFile) {
-            if(lastPath != "") {
-                request.uri += "/";
+                request.uri += defaultDocument;
             }
 
-            request.uri += defaultDocument;
+            console.log(request.uri);
         }
 
-        console.log(request.uri);
+        callback(null, request);
     }
-
-    callback(null, request);
 };
