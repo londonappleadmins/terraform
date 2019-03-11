b1f266a58cadd4f495ba24bf43567227755956e6
Graham Gilbert <graham.gilbert@airbnb.com>
2019-03-11 16:24:21 -0700

    Domain name

--- route53.tf
+++ route53.tf
@@ -26,7 +26,7 @@ resource "aws_route53_record" "laa_root" {
   name    = "londonappleadmins.org.uk"
 
   alias {
-    name                   = "${aws_cloudfront_distribution.www_distribution.name}"
+    name                   = "${aws_cloudfront_distribution.www_distribution.domain_name}"
     zone_id                = "${aws_cloudfront_distribution.www_distribution.hosted_zone_id}"
     evaluate_target_health = false
   }
