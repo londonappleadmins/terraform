646ab0731a4aba7ae617317843d13bbf08f212ad
Graham Gilbert <graham.gilbert@airbnb.com>
2019-03-11 11:56:14 -0700

    Switch to *.londonappleadmins.org.uk cert

--- cloudfront.tf
+++ cloudfront.tf
@@ -51,7 +51,7 @@ resource "aws_cloudfront_distribution" "www_distribution" {
   }
 
   viewer_certificate {
-    acm_certificate_arn = "arn:aws:acm:us-east-1:236095642140:certificate/e9c66c15-e23e-4fe4-a444-d0fc0fadd0d4"
+    acm_certificate_arn = "arn:aws:acm:us-east-1:236095642140:certificate/796d1944-5f9d-4b2b-abb9-f8b549570a48"
     ssl_support_method  = "sni-only"
   }
 }
